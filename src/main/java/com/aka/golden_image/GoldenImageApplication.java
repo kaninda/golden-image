cd297d0977e5cf0711f8691957ee18c870631ff8
kaninda <kanindaarnaud@gmail.com>
2026-08-15 11:33:12 +0200

    Spring Boot WAR + Tomcat externe

---
+++ src/main/java/com/aka/golden_image/GoldenImageApplication.java
@@ -0,0 +1,13 @@
+package com.aka.golden_image;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenImageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aka/golden_image/GoldenImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aka.golden_image;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
